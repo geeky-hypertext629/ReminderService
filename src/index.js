74fcc1190a6831b1cb17e84dc9e83ba49c5aa00b
geeky-hypertext629 <107639960+geeky-hypertext629@users.noreply.github.com>
2023-08-14 00:38:27 +0530

    Initial Setup Done

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+
+const {PORT} = require('./config/serverConfig');
+
+const setupAndStartServer = () =>{
+    const app = express();
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended : true}));
+
+    app.listen(PORT, ()=>{
+        console.log(`Server started on PORT : ${PORT}`);
+
+    })
+}
+
+setupAndStartServer();
