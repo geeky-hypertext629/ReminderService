8f8d5c3817b7eae70d93e451ebdd18050a17951b
geeky-hypertext629 <107639960+geeky-hypertext629@users.noreply.github.com>
2023-08-14 19:00:01 +0530

    Added notification ticket model

--- src/index.js
+++ src/index.js
@@ -3,7 +3,12 @@ const express = require('express');
 
 const {PORT} = require('./config/serverConfig');
 
-const {sendBasicEmail} = require('./services/email-service')
+const {sendBasicEmail} = require('./services/email-service');
+
+const cron = require('node-cron');
+
+
+
 
 const setupAndStartServer = () =>{
     const app = express();
@@ -11,12 +16,17 @@ const setupAndStartServer = () =>{
     app.use(bodyParser.urlencoded({extended : true}));
 
     app.listen(PORT, ()=>{
-        sendBasicEmail(
-            'support@admin.com',
-            'subh28909@gmail.com',
-            'This is a testing email',
-            'Hey, how are you , I like you like support',
-        );
+        //Syntax for sending email
+        // sendBasicEmail(
+        //     'support@admin.com',
+        //     'subh28909@gmail.com',
+        //     'This is a testing email',
+        //     'Hey, how are you , I like you like support',
+        // );
+
+        cron.schedule('*/2 * * * *', () => {
+            console.log('running a task every two minutes');
+          });
         console.log(`Server started on PORT : ${PORT}`);
      
     })
